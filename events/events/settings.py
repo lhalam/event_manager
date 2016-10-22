042c57fd23b2aa72d41a07b245cbe926fbaa0ebe
Roksolana Fedkovuch <roksolankafedkovuch@gmail.com>
2016-10-22 18:38:38 +0300

    Delete useless files

--- events/events/settings.py
+++ events/events/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'kjhgfdcvbnm,lkoijuytr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'static_precompiler',
-    'auth',
 ]
 
 MIDDLEWARE = [
