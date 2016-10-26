3c44536cdabac91ee0e9f343f74a782c9e320048
Lubko <lub4uk07@gmail.com>
2016-10-26 18:14:30 +0300

    conect calendar plugin with react components

---
+++ events/static/js/calendar.jsx
@@ -0,0 +1,11 @@
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Hello Wourld</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementsById('calendar'));
