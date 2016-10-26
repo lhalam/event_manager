e6b1bc4a7be48feee95127d6770c9406b1f8adc9
Roksolana Fedkovuch <roksolankafedkovuch@gmail.com>
2016-10-27 02:10:56 +0300

    Added views to profile and filemanager

--- events/profiles/models.py
+++ events/profiles/models.py
@@ -3,31 +3,27 @@ from django.db import models
 from django.conf import settings
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
+from datetime import datetime
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True)
-    bithday = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.CharField(max_length=200)
     homepage = models.URLField()
 
+    def create_user_profile(**kwargs):
+        UserProfile.objects.get_or_create(user=kwargs['user'])
 
-#def assure_user_profile_exists(pk):
- #   """
-  #  Creates a user profile if a User exists, but the
-   # profile does not exist.  Use this in views or other
-    #places where you don't have the user object but have the pk.
-    #"""
-    #user = User.objects.get(pk=pk)
-    #try:
-        # fails if it doesn't exist
-   #     userprofile = user.userprofile
-  #  except UserProfile.DoesNotExist, e:
- #       userprofile = UserProfile(user=user)
-#      userprofile.save()
- #   return
+    def __str__(self):
+        return "%s" % self.title
 
+    @staticmethod
+    def get_all():
+        return UserProfile.objects.all()
 
-#def create_user_profile(**kwargs):
-#    UserProfile.objects.get_or_create(user=kwargs['user'])
-
-#user_registered.connect(create_user_profile)
+    @staticmethod
+    def get_by_id(event_id):
+        try:
+            return UserProfile.objects.get(pk=event_id)
+        except UserProfile.DoesNotExist:
+            return None
--- events/profiles/views.py
+++ events/profiles/views.py
@@ -1,3 +1,53 @@
+import json
+from .models import User
+from boto.s3.key import Key
+from events import settings
 from django.shortcuts import render
+from boto.s3.key import Key
+from boto.s3.connection import S3Connection
+from django.http import JsonResponse
+from django.views.generic.base import View
 
-# Create your views here.
+class ProfileView(View):
+    def put(self, request, user_id):
+        if user_id.not_existing_users():
+            profile = [model_to_dict(data) for data in User.get_by_id(user_id)]
+            return JsonResponse(json.dumps(profile), status=200)
+        else:
+        	return JsonResponse({"not_existing_users": not_existing_users}, status=404)
+
+class FileManager(View):
+    def get(self, request):
+        key_bucket = self.get_key_bucket()
+        key_bucket.key = 'default_photo.png'
+        key_bucket.get_contents_to_filename(self.get_path)
+        return JsonResponse({'key_photo': key_bucket.key})
+
+    def post(self, request):
+        key_bucket = self.get_key_bucket()
+    	key_bucket.key = 'default_photo.png'
+        key_bucket.set_contents_from_filename(self.get_path)
+        return JsonResponse({'key_photo': k.key})
+
+    def delete(self, request):
+        key_bucket = self.get_key_bucket()
+        key_bucket.key = 'default_photo4.png'
+        try:
+            key_bucket.key.delete()
+            return JsonResponse({'photo_deleted': 'success'})
+        except:
+            return JsonResponse({'photo_deleted': 'error'})
+
+    def connect_to_S3(self):
+        conn = S3Connection(settings.ACCESS_KEY_ID, settings.SECRET_KEY) 
+        conn = S3Connection()
+        return conn
+
+    def get_path(self):
+        return '/home/roksa/Downloads/default_photo.png'
+
+    def get_key_bucket(self):
+        name_profiles_bucket = settings.NAME_PROFILES_BUCKET
+        bucket = self.connect_to_S3().get_bucket(name_profiles_bucket)
+        key_bucket = Key(bucket)
+        return key_bucket
