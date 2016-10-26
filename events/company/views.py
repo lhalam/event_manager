38de12d1b3d56cedebd852183f1fbaf43d21b5c6
Ihor Vlasiuk <grizly.vl@gmail.com>
2016-10-27 00:52:49 +0300

    add form validation for put and post methods for CompanyView and TeamView

--- events/company/views.py
+++ events/company/views.py
@@ -1,21 +1,22 @@
-# from django.shortcuts import render, reverse
-from .models import Company, TeamUserAssignment, Team, User
+import json
+
 from django.views.generic.base import View
 from django.http import HttpResponse, JsonResponse
-import json
+
+from .forms import CompanyForm, TeamForm
+from .models import Company, TeamUserAssignment, Team, User
 
 
 class CompanyView(View):
-
     def get(self, request, company_id=None):
         if not company_id:
             companies = Company.get_all()
             response = [{
-                'id': company.pk,
-                'name': company.name,
-                'description': company.description,
-                'company_admin': company.company_admin.username
-                         } for company in companies]
+                            'id': company.pk,
+                            'name': company.name,
+                            'description': company.description,
+                            'company_admin': company.company_admin.username
+                        } for company in companies]
             return JsonResponse(response, safe=False, status=200)
 
         company = Company.get_by_id(company_id)
@@ -32,28 +33,46 @@ class CompanyView(View):
 
     def post(self, request):
         new_company_data = json.loads(request.body.decode())
-        new_company_data['company_admin'] = User.objects.get(username=new_company_data.get('company_admin'))
+        company_form = CompanyForm(new_company_data)
+        if not company_form.is_valid():
+            return JsonResponse({'success': False,
+                                 'errors': company_form.errors}, status=400)
+        if new_company_data.get('company_admin'):
+            user = User.objects.get(username=new_company_data.get('company_admin'))
+            if Company.objects.filter(company_admin=user):
+                return JsonResponse({'success': False,
+                                     'errors': {
+                                         "company_admin": ['This user is already an admin of another company']
+                                     }
+                                     }, status=400)
+            new_company_data['company_admin'] = user
         company = Company(**new_company_data)
         company.save()
 
-        return HttpResponse(status=201)
+        return JsonResponse({'success': True}, status=201)
 
     def put(self, request, company_id):
-        data = json.loads(request.body.decode())
+        upd_company_data = json.loads(request.body.decode())
         company = Company.get_by_id(company_id)
-
         if not company:
             return JsonResponse({"error_message": "Such company does not exists"}, status=404)
-        if data.get('company_admin'):
-            data['company_admin'] = User.objects.get(username=data.get('company_admin'))
-            company.company_admin = data.get('company_admin')
-        if data.get('name'):
-            company.name = data.get('name')
-        if data.get('description'):
-            company.description = data.get('description')
 
+        company_form = CompanyForm(upd_company_data)
+        if not company_form.is_valid():
+            return JsonResponse({'success': False,
+                                 'errors': company_form.errors}, status=400)
+        if upd_company_data.get('company_admin'):
+            user = User.objects.get(username=upd_company_data.get('company_admin'))
+            if Company.objects.filter(company_admin=user):
+                return JsonResponse({'success': False,
+                                     'errors': {
+                                         "company_admin": ['This user is already an admin of another company']
+                                     }
+                                     }, status=400)
+            upd_company_data['company_admin'] = user
+        company = Company(**upd_company_data)
         company.save()
-        return HttpResponse(status=204)
+        return JsonResponse({'success': True}, status=201)
 
     def delete(self, request, company_id):
         Company.get_by_id(company_id).delete()
@@ -61,17 +80,16 @@ class CompanyView(View):
 
 
 class TeamView(View):
-
     def get(self, request, company_id, team_id=None):
         company = Company.get_by_id(company_id).name
         if not team_id:
             teams = Team.get_all()
             response = [{
-                'id': team.pk,
-                'name': team.name,
-                'company': company,
-                'members': Team.get_members(team)[:4]
-                         } for team in teams]
+                            'id': team.pk,
+                            'name': team.name,
+                            'company': company,
+                            'members': Team.get_members(team)[:4]
+                        } for team in teams]
             return JsonResponse(response, safe=False, status=200)
 
         team = Team.get_by_id(team_id)
@@ -82,39 +100,45 @@ class TeamView(View):
             'name': team.name,
             'company': company,
             'members': Team.get_members(team)
-                    }
+        }
         return JsonResponse(response, status=200)
 
     def post(self, request, company_id):
         new_team_data = json.loads(request.body.decode())
+        team_form = TeamForm(new_team_data)
+        if not team_form.is_valid():
+            return JsonResponse({'success': False,
+                                 'errors': team_form.errors}, status=400)
         team = Team(
             name=new_team_data.get('name'),
             company=Company.get_by_id(company_id)
-            )
+        )
         team.save()
         if new_team_data.get('members'):
             for username in new_team_data.get('members'):
                 user = User.objects.get(username=username)
                 TeamUserAssignment(user=user, team=team).save()
-        return HttpResponse(status=201)
+        return JsonResponse({'success': True}, status=201)
 
     def delete(self, request, company_id, team_id):
         Team.get_by_id(team_id).delete()
         return HttpResponse(status=204)
 
     def put(self, request, company_id, team_id):
-        team_upd_data = json.loads(request.body.decode())
+        upd_team_data = json.loads(request.body.decode())
+        team_form = TeamForm(upd_team_data)
+        if not team_form.is_valid():
+            return JsonResponse({'success': False,
+                                 'errors': team_form.errors}, status=400)
         team = Team.get_by_id(team_id)
         if not team:
             return JsonResponse({"error_message": "Such team does not exists"}, status=404)
-        if team_upd_data.get('name'):
-            team.name = team_upd_data.get('name')
+        team.name = upd_team_data.get('name')
         team.save()
-        return HttpResponse(status=204)
+        return JsonResponse({'success': True}, status=201)
 
 
 class TeamUserAssignmentView(View):
-
     def put(self, request, company_id, team_id):
         team_members = json.loads(request.body.decode())
         not_existing_users = []
