053c581c1803453e6db4a8a17f06cae70969d31f
sbufan <svitlana.bufan@gmail.com>
2016-11-14 23:42:58 +0000

    Fix urls.py

--- events/events/urls.py
+++ events/events/urls.py
@@ -1,29 +1,16 @@
-"""events URL Configuration
+"""events URL Configuration."""
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
     url(r'^$', login_required(TemplateView.as_view(template_name='main.html'), login_url='auth/login?next=/')),
+    url(r'^admin/', admin.site.urls),
     url(r'^auth/', include('auth.urls', namespace='auth')),
-    url(r'^api/v1/events/', include('event.urls', namespace='event')),
     url(r'^api/v1/reg/', include('registration.urls', namespace='reg')),
+    url(r'^api/v1/events/', include('event.urls', namespace='event')),
     url(r'^api/v1/calendars/', include('calendars.urls')),
     url(r'^api/v1/companies/', include('companies.urls', namespace='companies')),
 ]
