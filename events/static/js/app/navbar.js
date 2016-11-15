83b78095136a041ccb31963ee6f193f91e663857
Ihor Vlasiuk <grizly.vl@gmail.com>
2016-11-15 19:51:31 +0200

    add IndexRoute to router and Home tab.

--- events/static/js/app/navbar.js
+++ events/static/js/app/navbar.js
@@ -1,100 +1,34 @@
 import React from 'react';
 import { Router, Route, Link, browserHistory } from 'react-router';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
-
-
-import IconMenu from 'material-ui/IconMenu';
-import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
-import MenuItem from 'material-ui/MenuItem';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import RaisedButton from 'material-ui/RaisedButton';
-import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import {Tabs, Tab} from 'material-ui/Tabs';
+import FlatButton from 'material-ui/FlatButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-// class NavContainer extends React.Component {
-//     constructor(props) {
-//     super(props);
-//     this.state = {
-//       value: 3,
-//     };
-//     this.handleChange = (event, index, value) => this.setState({value});
-//   }
-//     render(){
-//         return (
-//             <Toolbar>
-//                 <ToolbarGroup firstChild={true}>
-//                     <DropDownMenu value={this.state.value} onChange={this.handleChange}>
-//                         <MenuItem value={1} primaryText="All Broadcasts" />
-//                         <MenuItem value={2} primaryText="All Voice" />
-//                         <MenuItem value={3} primaryText="All Text" />
-//                         <MenuItem value={4} primaryText="Complete Voice" />
-//                         <MenuItem value={5} primaryText="Complete Text" />
-//                         <MenuItem value={6} primaryText="Active Voice" />
-//                         <MenuItem value={7} primaryText="Active Text" />
-//                     </DropDownMenu>
-//                 </ToolbarGroup>
-//                 <ToolbarGroup>
-//                     <ToolbarTitle text="Options" />
-//                     <FontIcon className="muidocs-icon-custom-sort" />
-//                     <ToolbarSeparator />
-//                     <RaisedButton label="Create Broadcast" primary={true} />
-//                     <IconMenu
-//                         iconButtonElement={
-//                           <IconButton touch={true}>
-//                             <NavigationExpandMoreIcon />
-//                           </IconButton>
-//                         }
-//                     >
-//                         <MenuItem primaryText="Download" />
-//                         <MenuItem primaryText="More Info" />
-//                     </IconMenu>
-//                 </ToolbarGroup>
-//             </Toolbar>
-//         );
-//     }
-// }
+
 
 export default class Navbar extends React.Component {
 
-    render() {    // // constructor(props) {
-    // //     super(props);
-    // //     this.state = {
-    // //       value: 3,
-    // //     };
-    // //     this.handleChange = (event, index, value) => this.setState({value});
-    //   }
-        var styles = {
+    render() {
+
+        const styles = {
           tabs: {
-              width: '20%',
-              minWidth: '270px',
+              width: '35%',
+              minWidth: '200px',
           },
-            tab: {
-                marginTop: '-3px'
-            }
         };
-
-        const muiTheme = getMuiTheme({
-          appBar: {
-            height: 50, // Instead of 64
-          },
-        });
-
 
         return (
             <header>
-                <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
+                <MuiThemeProvider >
                     <AppBar
+                        showMenuIconButton={false}
                         title={
-
-
-                        <Tabs tabItemContainerStyle={styles.tab} onChange={this.onChangeTabs} style={styles.tabs}>
-                            <Tab label={<span className="navbar-tab">Calendar</span>} linkButton containerElement={<Link to="calendar" />} rippleColor="rgba(0,0,0,0)" />
-                            <Tab label={<span className="navbar-tab">Events</span>} linkButton containerElement={<Link to="event" />} rippleColor="rgba(0,0,0,0)" />
+                        <Tabs onChange={this.onChangeTabs} style={styles.tabs}>
+                            <Tab label="Home" containerElement={<Link to="/" />}/>
+                            <Tab label="Calendar" containerElement={<Link to="calendar" />}/>
+                            <Tab label="Events" containerElement={<Link to="event" />}/>
                         </Tabs>
                         }
                         iconElementRight={<FlatButton href="/auth/logout?next=/" label="Logout" />}
--- events/static/js/router.js
+++ events/static/js/router.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, Link, hashHistory } from 'react-router'
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router'
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
@@ -13,8 +13,8 @@ const Home = () => <div><h1>Home</h1></div>
 
 render(
       <Router history = { hashHistory }>
-          <Route component={ App }>
-              <Route path = "/" component ={ Home }/>
+          <Route path="/" component={ App }>
+              <IndexRoute component ={ Home }/>
               <Route path = "/calendar" component = { Calendar }/>
               <Route path = "/event" component ={ Events }/>
           </Route>
