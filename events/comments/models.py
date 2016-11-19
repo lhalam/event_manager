d340f1795a3b7f91956f805cd06cf9fd5539134a
Denys Abramov <abramovd90@gmail.com>
2016-11-19 15:51:22 +0200

    Create Comment model + Migrations

---
+++ events/comments/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from registration.models import User
+from event.models import Event
+
+# Create your models here.
+
+
+class Comment (models.Model):
+    author= models.ForeignKey(User, null=False)
+    event = models.ForeignKey(Event, null=True)
+    parrent_comment = models.ForeignKey('self', null=True, on_delete=models.CASCADE, related_name='children')
+    date_add = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(null=False)
+
+
+    def __str__(self):
+        return self.text
+
+    def to_dict(self):
+        children = [child.tgo_dict() for child in self.children.all()]
+        author = {
+            "id": self.author.id,
+            "name": "{} {}".format(self.author.first_name, self.author.last_name)
+        }
+        return {
+            "id": self.id,
+            "children": children,
+            "text": self.text,
+            "author": author,
+            "date": self.date_add.timestamp()
+        }
