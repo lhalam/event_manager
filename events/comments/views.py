1379b64ea989c52af5fa2c67133d3e5a82358d08
Denys Abramov <abramovd90@gmail.com>
2016-11-19 18:49:59 +0200

    Comment API
    Add Del Method

--- events/comments/views.py
+++ events/comments/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import View
+from event.models import Event
+from registration.models import User
 from .models import Comment
+from .forms import CommentForm
 
 import json
 
@@ -8,9 +11,21 @@ import json
 
 
 class CommentView(View):
-    def get(self, request, event_id):
+    def get(self, request, id):
         if not request.user.is_authenticated:
             return HttpResponse('Permission denied', status=403)
-        comments = Comment.objects.filter(event_id=event_id)
+        comments = Comment.objects.filter(event_id=id)
         response = [comment.to_dict() for comment in comments]
         return HttpResponse(json.dumps(response), content_type="text/json")
+
+    def post(self, request, id=None):
+        pass
+
+    def delete(self, request, id=None):
+        if not request.user:
+            return HttpResponse('Permission denied', status=403)
+        try:
+            Comment.objects.get(id=id).delete()
+        except:
+            return HttpResponse('Not Found', status=404)
+        return HttpResponse('Ok')
