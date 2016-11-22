fc501d709ed8b73c31876788bb29edc2bbd2586b
Ihor Vlasiuk <grizly.vl@gmail.com>
2016-11-22 20:26:52 +0200

    add changes to UI of company creation and edition

--- events/static/js/app/AddCompanyWindow.js
+++ events/static/js/app/AddCompanyWindow.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
 import CompanyTextField from './CompanyTextField';
-
+import axios from 'axios';
 export default class AddCompanyWindow extends React.Component {
 
     constructor(props) {
@@ -28,6 +29,12 @@ export default class AddCompanyWindow extends React.Component {
             })
         }
 
+    getAdminList()  {
+        axios.get('/api/v1/companies/get_admin/56')
+            .then((response) => {
+                console.log(response.data);
+            });
+    }
 
 
     render() {
@@ -50,13 +57,27 @@ export default class AddCompanyWindow extends React.Component {
             dialogBodyMenuStyle: {
                 maxHeight: '200px'
             },
-
         };
+
+            const standardActions = [
+                <FlatButton
+                    label="Cancel"
+                    primary={true}
+
+                />,
+                <FlatButton
+                    label="Submit"
+                    primary={true}
+
+                />,
+            ];
+
 
         return (
             <Dialog
                 open={this.props.open}
                 contentStyle={styles.dialogRoot}
+                actions={standardActions}
             >
                 <CompanyTextField
                     value='test'
@@ -73,8 +94,12 @@ export default class AddCompanyWindow extends React.Component {
                     rowsMax={5}
                     fullWidth={true}
                     onChange={this.handleDescription}
-
-                />
+                /><br />
+                <FlatButton
+                    label="Get admin list"
+                    primary={true}
+                    onTouchTap={this.getAdminList}
+                />,
             </Dialog>
         );
     }
