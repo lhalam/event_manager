2cc4bbb81727aad189380977f6754273fef3aa17
Denys Abramov <abramovd90@gmail.com>
2016-11-24 10:11:30 +0200

    TZ

--- events/event/models.py
+++ events/event/models.py
@@ -2,13 +2,12 @@ from django.db import models
 from registration.models import User
 from django.db.models.fields.related import ManyToManyField
 from django.contrib.postgres.fields import ArrayField
-from django.utils.timezone import utc
+from django.utils.timezone import utc as TZ
 
 from datetime import datetime
 
 
 
-TZ = utc
 
 
 class Event(models.Model):
