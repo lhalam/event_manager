171a865381a129781d2ae4f4c042eadb35981fdc
Ihor Vlasiuk <grizly.vl@gmail.com>
2016-12-20 11:44:28 +0200

    add styles to profile

--- events/static/js/app/Profile.js
+++ events/static/js/app/Profile.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import axios from 'axios';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import CircularProgress from 'material-ui/CircularProgress';
+import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+
 
 export default class Profile extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            error: '',
+            showButtonsPanel: false,
+            open: false
+        };
+        this.showButtons = () => this.setState({showButtonsPanel: true});
+        this.hideButtons = () => this.setState({showButtonsPanel: false});
+
+        this.openDialog =() => {this.setState({open: true});
+    };
+
+        this.getPhoto = this.getPhoto.bind(this)
     }
 
+    getPhoto() {
+        if (this.state.profile) {
+            return this.state.profile['photo'];
+        }
+        return <CircularProgress size={60} thickness={7} />;
+
+    }
+
+
     loadProfile() {
-        axios.get(`http://localhost:8000/api/v1/profile/${this.props.params.user_id ?
+        axios.get(`api/v1/profile/${this.props.params.user_id ?
                                                           this.props.params.user_id : ''}`)
             .then((response) => {
                 this.setState(response['data']);
@@ -26,16 +52,60 @@ export default class Profile extends React.Component {
     }
 
     render() {
-        if (this.state.error) return <h1>{this.state.error}</h1>;
-        let profile_photo = 'http://eventmanager203.s3.amazonaws.com/default_photo.jpg';
-        if (this.state.profile) {
-            profile_photo = this.state.profile['photo'];
-        }
+        if (this.state.error) return <h1 className="error-message">{this.state.error}</h1>;
+
+        let profile_photo = this.getPhoto();
+
         return (
-            <div>
-                <p>Worked!</p>
-                <img className="profile-pic" src={profile_photo} alt="userpic"/>
-           </div>
-            );
+            <MuiThemeProvider>
+                <div className="profile">
+
+                    <Paper className="picture-card">
+                        <div
+                            onMouseEnter={this.showButtons}
+                            className="image-container"
+                            onMouseLeave={this.hideButtons}
+                        >
+                             <div className="button-panel">
+                               <FlatButton
+                                 className="upload-picture"
+                                 label="upload photo"
+                                 onTouchTap={this.openDialog}
+                             />
+                            </div>
+                            <img className="profile-pic" src={profile_photo} alt="userpic"/>
+                        </div>
+                    </Paper>
+
+                    <Paper className="info-card">
+                        <p>
+                            1121212343 fg dfg dfg fdg fdg fdg
+                            f dg fdg
+                            dfg df
+                            g fdgfdgfdgfdgfdgfd1121212343 fg dfg dfg fdg fdg fdg
+                        </p>
+                        <p>
+                            1121212343 fg dfg dfg fdg fdg fdg
+                            f dg fdg
+                            dfg df
+                            g fdgfdgfdgfdgfdgfd1121212343 fg dfg dfg fdg fdg fdg
+                        </p>
+                        <p>
+                            1121212343 fg dfg dfg fdg fdg fdg
+                            f dg fdg
+                            dfg df
+                            g fdgfdgfdgfdgfdgfd1121212343 fg dfg dfg fdg fdg fdg
+                        </p>
+                    </Paper>
+
+                <Dialog
+                    open={this.state.open}
+                    actions={actions}
+                >
+                </Dialog>
+
+                </div>
+            </MuiThemeProvider>
+        );
     }
 }
