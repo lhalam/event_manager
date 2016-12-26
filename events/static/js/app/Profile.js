ae64cbf3dd030634cfa83668b94f3926f1e7f9b4
Ihor Vlasiuk <grizly.vl@gmail.com>
2016-12-26 19:30:32 +0200

    add ability to change avatar

--- events/static/js/app/Profile.js
+++ events/static/js/app/Profile.js
@@ -6,9 +6,8 @@ import CircularProgress from 'material-ui/CircularProgress';
 import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
-
-let Dropzone = require('react-dropzone');
-let FileInput = require('react-file-input');
+import {Cropper} from 'react-image-cropper';
+import Dropzone from 'react-dropzone';
 
 
 export default class Profile extends React.Component {
@@ -18,56 +17,90 @@ export default class Profile extends React.Component {
             error: '',
             showButtonsPanel: false,
             open: false,
-            uploadedImage: false
+            openDel: false,
+            uploadedImage: false,
         };
+
         this.showButtons = () => this.setState({showButtonsPanel: true});
         this.hideButtons = () => this.setState({showButtonsPanel: false});
         this.openDialog = () => this.setState({open: true});
         this.closeDialog = () => this.setState({open: false, uploadedImage: false});
+        this.openDelDialog = () => this.setState({openDel: true});
+        this.closeDelDialog = () => this.setState({openDel: false});
 
-        this.onDrop = (files) => {
-            console.log('Received files: ', files);
-            this.sendFile(files[0], 'api/v1/profile/photo/');
-            this.setState({uploadedImage: files[0]});
+        this.crop = () => {
+            let image = this.state.uploadedImage;
+            let croppedImage = this.dataURItoFile(
+                this.refs['cropper'].crop(),
+                `${this.state.profile.user.id}-${image.name}`,
+                image.type
+            );
+            console.log(croppedImage);
+            this.sendPhoto(croppedImage, 'api/v1/profile/photo/');
         };
 
-        this.handleFileUpload = (e) => {
-            e.preventDefault();
-            let file = e.target.files[0];
-            console.log('Received file: ', file);
-            this.sendFile(file, 'api/v1/profile/photo/');
-            this.setState({uploadedImage: file[0]});
-        };
-
-        this.sendFile = this.sendFile.bind(this);
-        this.getPhoto = this.getPhoto.bind(this);
+        this.sendPhoto = this.sendPhoto.bind(this);
+        this.deletePhoto = this.deletePhoto.bind(this);
     }
 
-    getPhoto() {
-        if (this.state.profile) {
-            return this.state.profile['photo'];
+    dataURItoFile(dataURI, imageTitle, mimeString) {
+        let byteString = atob(dataURI.split(',')[1]);
+        let ab = new ArrayBuffer(byteString.length);
+        let ia = new Uint8Array(ab);
+        for (let i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
         }
-        return <CircularProgress size={60} thickness={7} />;
+        let blob = new Blob([ab], {type: mimeString});
+        console.log(blob);
+        let file = new File([blob], imageTitle);
+        console.log(file);
+        return file;
+    }
+
+    deletePhoto() {
+        this.closeDelDialog();
+        this.closeDialog();
+
+        axios.delete(`api/v1/profile/${this.state.profile.user.id}`)
+            .then((response) => {
+                console.log('response', response.data);
+                let profile = this.state.profile;
+                profile['photo'] = response.data['photo'];
+                profile['key'] = response.data['key'];
+                this.setState({
+                    profile: profile,
+
+                })
+            })
+            .catch((error) => {
+                console.log(error.response)
+            })
 
     }
 
-    sendFile(received_file, URL) {
+    sendPhoto(received_file, URL) {
+        console.log('received_file', received_file);
         let file = new FormData();
         file.append('profile_pic', received_file);
-        console.log(file);
+        console.log('file', file);
         axios.post(URL, file)
             .then((res) => {
-                console.log(res)
+                let profile = this.state.profile;
+                profile['photo'] = res.data['photo'];
+                profile['key'] = received_file.name;
+                this.setState({
+                    profile: profile,
+                    open: false,
+                    uploadedImage: false
+                })
             })
             .catch((err) => {
                 console.log(err.response)
             });
     };
 
-
-    loadProfile() {
-        axios.get(`api/v1/profile/${this.props.params.user_id ?
-                                                          this.props.params.user_id : ''}`)
+    loadProfile(url) {
+        axios.get(url)
             .then((response) => {
                 this.setState(response['data']);
             })
@@ -77,23 +110,59 @@ export default class Profile extends React.Component {
             })
     }
 
-    componentDidMount() {
+    getUrl() {
+        let parameter = this.props.params.user_id ? this.props.params.user_id.toString() : '';
+        return 'api/v1/profile/'+parameter;
+    }
 
-        this.loadProfile();
-        
+    componentWillMount() {
+        let url = this.getUrl();
+        this.loadProfile(url);
     }
 
     render() {
         if (this.state.error) return <h1 className="error-message">{this.state.error}</h1>;
-
-        let profile_photo = this.getPhoto();
         const actions = [
+            this.state.uploadedImage ?
+                <FlatButton
+                label="Set photo"
+                disabled={!(this.state.uploadedImage)}
+                primary={true}
+                onTouchTap={this.crop}
+                /> : null,
+            this.state.profile &&
+            !this.state.uploadedImage &&
+            this.state.profile.key != "default_photo.jpg" ?
+                <RaisedButton
+                className="delete-button"
+                label="Delete photo"
+                secondary={true}
+                onTouchTap={this.openDelDialog}
+                /> : null,
             <FlatButton
                 label="Close"
                 primary={true}
                 onTouchTap={this.closeDialog}
-            />
+            />,
+
         ];
+
+        let dialogContent = (this.state.uploadedImage ?
+                <Cropper
+                    className="pic-to-crop"
+                    src={this.state.uploadedImage.preview}
+                    ref="cropper"
+                    allowNewSelection={false}
+                /> :
+            <Dropzone
+                accept="image/*"
+                ref="dropzone"
+                onDrop={(files) => this.setState({uploadedImage: files[0]}).bind(this)}
+            >
+                <div>Try dropping some files here, or click to select files to upload.</div>
+            </Dropzone>
+        );
+
         return (
             <MuiThemeProvider>
                 <div className="profile">
@@ -104,66 +173,65 @@ export default class Profile extends React.Component {
                             className="image-container"
                             onMouseLeave={this.hideButtons}
                         >
-                             <div className="button-panel">
-                               <FlatButton
-                                 className="upload-picture"
-                                 label="upload photo"
-                                 onTouchTap={this.openDialog}
-                             />
-                            </div>
-                            <img className="profile-pic" src={profile_photo} alt="userpic"/>
+                            { this.state['owner'] ?
+                                <div className="button-panel">
+
+                                    <FlatButton
+                                        className="upload-picture"
+                                        label={
+                                            this.state.profile.key == 'default_photo.jpg' ?
+                                                "upload photo" : "change photo"
+                                        }
+                                        onTouchTap={this.openDialog}
+                                    />
+
+                                </div> : null
+                            }
+                            {
+                                this.state.profile ?
+                                    <img className="profile-pic" src={this.state.profile['photo']} alt="userpic"/>
+                                     :
+                                    <CircularProgress size={60} thickness={7} />
+                            }
+
                         </div>
                     </Paper>
 
                     <Paper className="info-card">
-                        <p>
-                            1121212343 fg dfg dfg fdg fdg fdg
-                            f dg fdg
-                            dfg df
-                            g fdgfdgfdgfdgfdgfd1121212343 fg dfg dfg fdg fdg fdg
-                        </p>
-                        <p>
-                            1121212343 fg dfg dfg fdg fdg fdg
-                            f dg fdg
-                            dfg df
-                            g fdgfdgfdgfdgfdgfd1121212343 fg dfg dfg fdg fdg fdg
-                        </p>
-                        <p>
-                            1121212343 fg dfg dfg fdg fdg fdg
-                            f dg fdg
-                            dfg df
-                            g fdgfdgfdgfdgfdgfd1121212343 fg dfg dfg fdg fdg fdg
-                        </p>
                     </Paper>
 
-                <Dialog
-                    open={this.state.open}
-                    actions={actions}
-                >
-                    { this.state.uploadedImage ?
-                            <img src={this.state.uploadedImage.preview} alt="userpic"/>
-                         :
-                            <div>
+                    <Dialog
+                        open={this.state.open}
+                        actions={actions}
+                        contentClassName="dialog-image-window"
+                    >
+                       {dialogContent}
+                    </Dialog>
 
-                            <RaisedButton
-                            type="primary"
-                            label="upload photo"
-                            className="upload-photo-button"
-                            >
-                                <FileInput
-                                    name="myImage"
-                                    accept=".png,.gif,.jpg"
-                                    placeholder="My Image"
-                                    className="file-upload"
-                                    onChange={this.handleFileUpload}
-                                />
-                            </RaisedButton>
-                            <Dropzone ref="dropzone" onDrop={this.onDrop}>
-                                <div>Try dropping some files here, or click to select files to upload.</div>
-                            </Dropzone>
-                            </div>
-                    }
-                </Dialog>
+                    <Dialog
+                        actions={
+                            [
+                                <FlatButton
+                                    label="Cancel"
+                                    primary={true}
+                                    onTouchTap={this.closeDelDialog}
+                                />,
+                                <RaisedButton
+                                    className="delete-button"
+                                    label="Delete"
+                                    primary={true}
+                                    style={{backgroundColor: "#f44336", color: "white", marginLeft: "10px"}}
+                                    onTouchTap={this.deletePhoto}
+                                />,
+                            ]
+                        }
+                        modal={true}
+                        open={this.state.openDel}
+                        onRequestClose={this.closeDelDialog}
+                        contentClassName="dialog-window"
+                    >
+                        Are you sure you want delete the voting?
+                    </Dialog>
 
                 </div>
             </MuiThemeProvider>
